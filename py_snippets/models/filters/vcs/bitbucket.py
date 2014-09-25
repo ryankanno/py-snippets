22f5be192f09622a3d1c6d6f975e84f4fde784f8
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-09-25 07:26:40 -0400

    Added bitbucket/git filters

--- py_snippets/models/filters/vcs/bitbucket.py
+++ py_snippets/models/filters/vcs/bitbucket.py
@@ -3,9 +3,22 @@
 
 from .base import VcsLinkFilter
 
+
 class BitBucketLinkFilter(VcsLinkFilter):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, repo, *args, **kwargs):
         super(BitBucketLinkFilter, self).__init__(*args, **kwargs)
+        self.repo = repo
+
+    @property
+    def repo(self):
+        return self._repo
+
+    @repo.setter
+    def repo(self, value):
+        self._repo = value
+
+    def get_vcs_link_format(self):
+        return "https://bitbucket.org/" + self.repo + "/commits/{0}"
 
 # vim: filetype=python
--- py_snippets/models/filters/vcs/github.py
+++ py_snippets/models/filters/vcs/github.py
@@ -6,7 +6,19 @@ from .base import VcsLinkFilter
 
 class GithubLinkFilter(VcsLinkFilter):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, repo, *args, **kwargs):
         super(GithubLinkFilter, self).__init__(*args, **kwargs)
+        self.repo = repo
+
+    @property
+    def repo(self):
+        return self._repo
+
+    @repo.setter
+    def repo(self, value):
+        self._repo = value
+
+    def get_vcs_link_format(self):
+        return "https://github.com/{0}/commit/{0}"
 
 # vim: filetype=python
